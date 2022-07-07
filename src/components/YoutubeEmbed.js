27f4fc3396d4c741b35861ae11689f5e6fd93a0e
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2022-07-07 17:35:53 -0400

    made youtube frames responsive

--- src/components/YoutubeEmbed.js
+++ src/components/YoutubeEmbed.js
@@ -3,16 +3,24 @@ import PropTypes from "prop-types";
 
 function YoutubeEmbed({embedId}) {
   return (
-    <div className="video-responsive">
+    <div className="video-container">
         <iframe
-        width="540"
-        height="305"
+        // width="540"
+        // height="305"
         src={`https://www.youtube.com/embed/${embedId}`}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="Embedded youtube"
-        />
+        frameborder="0" 
+        allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen
+        style={{
+            position: "relative",
+            top: 0,
+            left: 0,
+            width: "90vw",
+            height: "50.83vw",
+            maxWidth: "600px",
+            maxHeight: "337.5px"
+        }}
+        ></iframe>
     </div>
   )
 }
