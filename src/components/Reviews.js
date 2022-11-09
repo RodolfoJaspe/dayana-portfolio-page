6a8674911c698b28976bc6f1d3a8d9e8ef1199ee
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2022-11-09 14:38:38 -0500

    fixed review typo

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -10,7 +10,7 @@ export default function Reviews() {
             <i>-Bill Hirschman, <a className='review-link' href='http://www.floridatheateronstage.com/reviews/theatre-labs-red-riding-hood-entertains-more-than-just-kids/' target='_blank'>Florida Theater on Stage </a></i> 
         </p><br/>
         <p>
-            “...It is the winsome Morales, a junior in FAU’s undergraduate theater program, who steals the show. As the stagestruck Delivery Person, her initial hesitance is quickly erased and she soon throws herself into full-throttle performance mode with evident glee. Despite their natural antagonism a as Red and the Wolf, Morales and Davidson play off each other nimbly” <br/><br/>
+            “...It is the winsome Morales, a junior in FAU’s undergraduate theater program, who steals the show. As the stagestruck Delivery Person, her initial hesitance is quickly erased and she soon throws herself into full-throttle performance mode with evident glee. Despite their natural antagonism as Red and the Wolf, Morales and Davidson play off each other nimbly” <br/><br/>
             <i>-Hap Erstein, <a className='review-link' href='http://palmbeachartspaper.com/fau-theatre-labs-retake-on-red-riding-hood-a-little-crowd-pleaser/' target='_blank'>Palm Beach Arts Paper</a>  </i>
         </p><br/>
         <p>
