98261a04b6402adf5cb6de58728ca16d31161253
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2022-11-28 22:27:11 -0500

    railway deploy url

--- src/Assets/urls.js
+++ src/Assets/urls.js
@@ -1,3 +1,3 @@
 const localUrl = "http://localhost:5000";
-const deployUrl = "https://dayana-portfolio.herokuapp.com";
+const deployUrl = "https://dayana-portfolio-production.up.railway.app";
 export const currentUrl = deployUrl
