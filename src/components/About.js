63daa17958c90d8668628c4316979d054398db6c
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2023-04-20 13:34:31 -0400

    back to hardcoded text while debugging

--- src/components/About.js
+++ src/components/About.js
@@ -40,15 +40,15 @@ function About({biography, getBiography}) {
         </div>
         <div className='banner-about-div'>
             <h2>ABOUT ME</h2>
-            <Text />
+            {/* <Text /> */}
             {/* <p>{biography}</p> */}
   
-                {/* <p>
+                <p>
                     Dayana Morales is a Venezuelan bilingual stage and screen actress, last seen playing Eurydice in <i>Polaroid Stories</i> at Studio One Theatre FAU. She discovered her love for acting while studying journalism and decided to leave her country to train herself and pursue her passion. She is working towards her BFA in Acting from Florida Atlantic University and has a background in modern dance, contemporary dance and pole dance. She has training in Droznin Russian Movement with the National Theater Institute and Stage Combat with the Society of American Fight Directors. 
                 </p>
                 <p>
                     Credits Include: Delivery Person in <i>Red Riding Hood</i> (TheatreLab), <i>ShortCuts Tour</i> (CityTheatre Miami), Mimi in <i>RENT </i>(FAU's Festival Repertory) <i>Highlights from Romeo and Juliet</i> and <i>A Midsummer Night's Dream</i> (Shakespeare Troupe), <i>The Office "A Lo Miami"</i> (Microtheater Miami), <i>Prelude 2100</i> (Miami Live Arts-Deering Estate), Thea in <i>Hedda Gabler</i> (Florida Atlantic University) and Antonia in <i>Twelfth Night</i> (PalmBeach Shakespeare)    
-                </p> */}
+                </p>
         
                 
             <button className="resume-button"onClick={() => setPopup(!popup)}>Full Resume</button>
