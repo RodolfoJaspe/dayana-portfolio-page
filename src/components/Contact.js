61f72606e7e812bc266f91eaddee82f7303a58c4
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2024-01-06 16:53:25 -0500

    emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,7 +27,7 @@ function Contact() {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_h6yxzhl', 'template_496smvb', form.current, '3fjWoU26gcULc4m0c')
+        emailjs.sendForm('service_h6yxzhl', 'template_496smvb', form.current, 'MbMoon4mkf9qhvRFuwr-K')
         .then((result) => {
             console.log(result.text);
         }, (error) => {
