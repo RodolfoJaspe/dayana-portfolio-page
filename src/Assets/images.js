6ac34582264612ca25bb6b2bf367d1e45f005eee
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2024-02-03 15:23:02 -0500

    updated images

--- src/Assets/images.js
+++ src/Assets/images.js
@@ -4,8 +4,9 @@ export const pageImages =[
         url: "https://lh3.googleusercontent.com/pw/ABLVV84fY7fjtg_EBCldY0vYpZy2pbssc73pIcpxHTKxlrpxoqIC3YbhiSLv1LZmxfi13ROqNswhk12Z61dnVpmm5IwBFo58tdJaYG1AwCD6_93BJ40LjtR6agzTir35U9momzzbjaDZKaLa6bsgZBcKc5jq=w2664-h2130-s-no-gm?authuser=0"},
     {   id: 2,
         title: "About Headshot",
-        url: "https://lh3.googleusercontent.com/pw/ABLVV85i1DyohQiiz1iB9vyLsTU7sQW7EeXGsiOlOU77YzgWRz0oen6WMSmFtD9ib0bpyDCSN8FiG0janalLU9odYQFboVzMhWoZAoajWLGmT3ZvcPve5fwbeeuODc0Js3Gjty14PRXpIzF45hGT15RlChpm=w1704-h2130-s-no-gm?authuser=0"},
+        url: "https://lh3.googleusercontent.com/pw/ABLVV86V2VAZX7-uT6fG8hU7pEgGhrWJALCt_nzB77N3ZWfdPbLp1XmqmZ__2dfmZRmklymLhkzk9M8qUc4ovU8g7tjukj_A4Z8rbnRlrImaIRgI-mXnzTT_MD4KbOHkWMMQQshFH3UqX7KCqm2zg9hJ6aTm=w1704-h2130-s-no-gm?authuser=0"},
     {   id: 3,
         title: "Contact Headshot",
-        url: "https://lh3.googleusercontent.com/pw/ABLVV86V2VAZX7-uT6fG8hU7pEgGhrWJALCt_nzB77N3ZWfdPbLp1XmqmZ__2dfmZRmklymLhkzk9M8qUc4ovU8g7tjukj_A4Z8rbnRlrImaIRgI-mXnzTT_MD4KbOHkWMMQQshFH3UqX7KCqm2zg9hJ6aTm=w1704-h2130-s-no-gm?authuser=0"},
+        url: "https://lh3.googleusercontent.com/pw/ABLVV85i1DyohQiiz1iB9vyLsTU7sQW7EeXGsiOlOU77YzgWRz0oen6WMSmFtD9ib0bpyDCSN8FiG0janalLU9odYQFboVzMhWoZAoajWLGmT3ZvcPve5fwbeeuODc0Js3Gjty14PRXpIzF45hGT15RlChpm=w1704-h2130-s-no-gm?authuser=0"},
     ];
+    
